1b9a8994ee270fe76142986c97407d0006462869
RezaAzharii <rezaazhari867@gmail.com>
2025-05-08 23:37:23 +0700

    Menambahkan BlocBuilder untuk menampilkan dateselect

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todobloc_app/bloc/todo_bloc.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final _key = GlobalKey<FormState>();
-    final _controller = TextEditingController();
+    final key = GlobalKey<FormState>();
+    final controller = TextEditingController();
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -18,14 +20,26 @@ class TodoPage extends StatelessWidget {
                 children: [
                   Column(
                     children: [
-                      Text('Selected Date')
+                      Text('Selected Date'),
+                      BlocBuilder<TodoBloc, TodoState>(
+                        builder: (context, state) {
+                          if (state is TodoLoaded) {
+                            if (state.selectedDate != null) {
+                              return Text(
+                                '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                              );
+                            }
+                          }
+                          return Text('No Date Selected');
+                        },
+                      ),
                     ],
-                  )
+                  ),
                 ],
-              )
+              ),
             ],
           ),
-        ), 
+        ),
       ),
     );
   }
